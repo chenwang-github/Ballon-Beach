a7a7f7fd9d0a1836fb1baed0ee9a887b3c923fb5
chenwang <cwang19@uci.edu>
2019-01-06 23:38:20 -0800

    playButton Complete

--- Assets/Script/AppInit.cs
+++ Assets/Script/AppInit.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class AppInit : MonoBehaviour {
 
+    public GameObject inMenuUI;
+    public GameObject inGameUI;
+    public GameObject gameOverUI;
+    public GameObject player;
+
+
     void Awake()
     {
         Shader.SetGlobalFloat("_Curvature", 2.0f);
         Shader.SetGlobalFloat("_Trimming", 0.1f);
+        Application.targetFrameRate = 60;
     }
 
     // Use this for initialization
     void Start () {
-		
+        player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+        inMenuUI.gameObject.SetActive(true);
+        inGameUI.gameObject.SetActive(false);
+        gameOverUI.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void playButton(){
+        StartCoroutine(StartGame(0.0f));
+    }
+
+    IEnumerator StartGame(float waitTime){
+        inMenuUI.gameObject.SetActive(false);
+        inGameUI.gameObject.SetActive(true);
+        gameOverUI.gameObject.SetActive(false);
+        yield return new WaitForSeconds(waitTime);
+        player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+    }
 }
